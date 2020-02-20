137d41a694f11599e8801230573bbc1fa49183da
solokwal <lea.solo-kwan@grenoble-inp.fr>
2020-02-20 16:49:57 +0100

    version tache3v2

--- src/com/ensimag/Models/PlateWithCoords.java
+++ src/com/ensimag/Models/PlateWithCoords.java
@@ -3,5 +3,13 @@ package com.ensimag.Models;
 /**
  * Created by solokwal on 2/20/20.
  */
-public class PlateWithCoords {
+public class PlateWithCoords extends Plate{
+    private int x;
+    private int y;
+
+    public PlateWithCoords(Plate _plate,int _x, int _y) {
+        super(_plate.getH(), _plate.getW());
+        this.x = _x;
+        this.y = _y;
+    }
 }
