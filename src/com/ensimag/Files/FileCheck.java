bdb58ddd0800602ebd5c064fbba604625dd3986d
Unknown <lea.solo@etu.unilim.fr>
2020-03-14 10:42:37 +0100

    New classes

---
+++ src/com/ensimag/Files/FileCheck.java
@@ -0,0 +1,13 @@
+package com.ensimag.Files;
+
+import com.ensimag.Models.Plate;
+import com.ensimag.Models.Rectangle;
+
+import java.util.Map;
+
+public class FileCheck {
+    
+    private String fileName;
+    private Map<Plate, Integer> plates;
+    private Map<Rectangle, Integer> pieces;
+}
---
+++ src/com/ensimag/Utils/Checker.java
@@ -0,0 +1,7 @@
+package com.ensimag.Utils;
+
+public class Checker {
+    public void start(){
+
+    }
+}
